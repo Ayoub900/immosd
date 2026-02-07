242065150bd0f3aaaa44db09ac8f47b3f2b50913
Ayoub900 <k.kassimi9009@gmail.com>
2026-02-07 20:07:50 +0100

    attempt to fix the prooduction login error

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
     database: prismaAdapter(prisma, {
         provider: "mongodb",
     }),
@@ -13,11 +14,24 @@ export const auth = betterAuth({
     session: {
         expiresIn: 60 * 60 * 24 * 7, // 7 days
         updateAge: 60 * 60 * 24, // 1 day (update session every day)
+        cookieCache: {
+            enabled: true,
+            maxAge: 5 * 60, // 5 minutes
+        },
     },
     advanced: {
         database: {
             generateId: false, // Let MongoDB generate ObjectId automatically
         },
+        cookiePrefix: "better-auth",
+        useSecureCookies: process.env.NODE_ENV === "production",
+        crossSubDomainCookies: {
+            enabled: true,
+        },
     },
-    trustedOrigins: [process.env.BETTER_AUTH_URL || "http://localhost:3000"],
+    trustedOrigins: [
+        process.env.BETTER_AUTH_URL || "http://localhost:3000",
+        "https://immobiliercharkaoui.com",
+        "http://localhost:3000",
+    ],
 });
